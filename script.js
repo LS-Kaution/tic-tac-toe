b8525363a3df476e7b52f47d53ebb3b15005b055
LS-Kaution <leonardosaturnoplaneta@gmail.com>
2022-11-13 00:41:50 -0500

    fix in script.js selected characters addEventListener

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ const header = document.querySelector('header')
 const footer = document.querySelector('footer')
 const main = document.querySelector('main')
 
+const imgs = document.querySelectorAll('.img')
+
+
+for(const img of imgs) {
+    img.addEventListener('click', () => {
+        selectedCharacter(img)
+    }) 
+}
+
+
+const buttonStartGame = document.querySelector('#button-start-game')
+buttonStartGame.addEventListener('click', callPreGame)
 
 const chooseTitle = document.querySelector('#choose-title')
 chooseTitle.style.display = 'none'
@@ -11,15 +23,19 @@ const container = document.querySelector('#container')
 container.style.display = 'none'
 
 
-const buttonStartGame = document.querySelector('#button-start-game')
-buttonStartGame.addEventListener('click', () => {
+function callPreGame() {
     header.style.display = 'none'
     footer.classList.toggle('click')
     main.classList.toggle('click')
     buttonStartGame.style.display = 'none'
     chooseTitle.style.display = 'flex'
     container.style.display = 'flex'
-}) 
+} 
+
+
+function selectedCharacter(element) {
+    element.classList.toggle('selected')
+}
 
 
 function resetGame() {
